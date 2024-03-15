9d979e0e59832febfb0e7ef4ff05a9bb9ed20509
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-03-15 21:08:35 +0530

    feat: example controller

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,13 @@ func SetupRoutes(r *gin.Engine, db *sql.DB) {
 	{
 		exampleRoutes := api.Group("/example") // example api group
 		{
-			exampleRoutes.GET("/", controllers.GetExample(db)) // get all examples
+			exampleRoutes.GET("/", controllers.GetExample(db))               // get all examples
+			exampleRoutes.GET("/:id", controllers.GetExampleByID(db))        // get example by ID
+			exampleRoutes.POST("/", controllers.AddExample(db))              // add example
+			exampleRoutes.PUT("/:id", controllers.UpdateExample(db))         // update example by id
+			exampleRoutes.PUT("/bulk", controllers.UpdateExampleBulk(db))    // update examples (bulk) by id
+			exampleRoutes.DELETE("/:id", controllers.DeleteExample(db))      // update examples (bulk) by id
+			exampleRoutes.DELETE("/bulk", controllers.DeleteExampleBulk(db)) // update examples (bulk) by id
 		}
 	}
 }
